06ab629b82917a556ffe91f61ce62d130b15cdde
Maciej Koson <kosonmaciej@gmail.com>
2017-01-05 23:18:30 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <title>Koson's cleaning services</title>
+
+    <meta name="description" content="Professional cleaning houses, apartments and offices in London. We clean all kinds of carpets
+       , sofas, beds and couchs.">
+    <meta name="keywords" content="Cleaning services, cleaning london, cleaning houses,apartments">
+    <meta name="author" content="Maciej Koson">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!--    <link rel="stylesheet" type="text/css" href="style.css">-->
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top" id="my-navbar">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
+            <a href="" class="navbar-brand">Cleaning services</a>
+        </div><!-- Navbar Header-->
+        <div class="collapse navbar-collapse" id="navbar-collapse">
+
+            <a href="" class="btn btn-info navbar-btn navbar-right">Download app</a>
+
+            <ul class="nav navbar-nav">
+                <li><a href="#feedback">Feedback</a>
+                <li><a href="#gallery">Gallery</a>
+                <li><a href="#features">Features</a>
+                <li><a href="#faq">Faq</a>
+                <li><a href="#contact">ContactUs</a>
+            </ul>
+        </div>
+    </div><!-- End Container-->
+</nav><!-- End navbar -->
+
+<!-- Jumbotron -->
+
+<div class="jumbotron">
+    <div class="container text-center" >
+        <h1>Cleaning services info</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquet lectus ante, at gravida ante ultricies non. Donec enim elit, malesuada id purus non, mattis consectetur velit. Maecenas mattis dolor non tempor ultricies. Sed semper dui at erat pretium, nec iaculis libero porttitor. Fusce ac ultricies justo.</p>
+
+        <div class="btn-group">
+            <a href="" class="btn btn-lg btn-info">Our job</a>
+            <a href="" class="btn btn-lg btn-info">Info</a>
+            <a href="" class="btn btn-lg btn-info">Our people</a>
+        </div>
+    </div> <!-- end of container -->
+</div>
+<!-- End jumbotron -->
+
+
+
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
