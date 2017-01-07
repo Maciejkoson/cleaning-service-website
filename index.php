7731ab3085a2597abd151f139ba9fe0c5110b38c
Maciej Koson <kosonmaciej@gmail.com>
2017-01-07 11:53:10 +0100

    Added gallery stuff

--- index.php
+++ index.php
@@ -119,12 +119,68 @@
                     <label for="subscription">Subscribe</label>
                     <input type="text" class="form-control" id="subscription" placeholder="Your name">
                 </div>
+                <div class="form-group">
+                    <label for="email">Email address</label>
+                    <input type="text" class="form-control" id="email" placeholder="Your email address">
+                </div>
+                <button type="submit" class="btn btn-default">Subscribe now!</button>
+                <hr>
             </form>
         </div> <!-- end container -->
 
     </div> <!-- end well -->
 
-</section>
+</section> <!-- call to action section end -->
+
+<!-- gallery section -->
+<div class="container">
+    <section>
+        <div class="page-header" id="gallery">
+            <h2>Gallery.<small>Check out our gallery!</small></h2>
+        </div>
+
+        <div class="carousel slide" id="screenshot-carousel" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#screenshot-carousel" data-slide-to="0" class="active"></li>
+                <li data-target="#screenshot-carousel" data-slide-to="1"></li>
+                <li data-target="#screenshot-carousel" data-slide-to="2"></li>
+                <li data-target="#screenshot-carousel" data-slide-to="3"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="item ">
+                    <img src="images/office-cleaning.jpg" alt="office-leaning">
+                    <div class="carousel-caption">
+                        <h2>Office cleaninig</h2>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="images/london-apartment-cleaning-service.jpg" alt="apartment-cleaning">
+                    <div class="carousel-caption">
+                        <h2>Apartment cleaninig</h2>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="images/restaurant-cleaning.jpg" alt="restaurant-cleaning">
+                    <div class="carousel-caption">
+                        <h2>Restaurant cleaning</h2>
+                    </div>
+                </div>
+                <div class="item active">
+                    <img src="images/cleaning-services-london.jpg" alt="cleaning-services-london">
+                    <div class="carousel-caption">
+                        <h2>Cleaning services in London!</h2>
+                    </div>
+                </div>
+            </div> <!-- end of carousel inner -->
+            <a href="#screenshot-carousel" class="left carousel-control" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+                </a>
+            <a href="#screenshot-carousel" class="right carousel-control" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+            </a>
+        </div> <!-- end carousel -->
+    </section>
+</div>
 
     <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
 
