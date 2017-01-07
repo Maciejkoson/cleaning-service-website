31d400b7cb8da5e0fd5df87a396d98dc3f43e5d6
Maciej Koson <kosonmaciej@gmail.com>
2017-01-07 12:53:13 +0100

    Added another section

--- index.php
+++ index.php
@@ -27,6 +27,7 @@
     body
     {
         padding-top: 40px;
+        background-color: lightgrey;
     }
 </style>
 <body>
@@ -49,9 +50,9 @@
             <ul class="nav navbar-nav">
                 <li><a href="#feedback">Feedback</a>
                 <li><a href="#gallery">Gallery</a>
-                <li><a href="#features">Features</a>
+                <li><a href="#Job">Our job</a>
                 <li><a href="#faq">Faq</a>
-                <li><a href="#contact">ContactUs</a>
+                <li><a href="#contact">Contact us</a>
             </ul>
         </div>
     </div><!-- End Container-->
@@ -61,7 +62,7 @@
 
 <div class="jumbotron">
     <div class="container text-center" >
-        <h1>Cleaning services info</h1>
+        <h1>Fast and cheap cleaning services in London!</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquet lectus ante, at gravida ante ultricies non. Donec enim elit, malesuada id purus non, mattis consectetur velit. Maecenas mattis dolor non tempor ultricies. Sed semper dui at erat pretium, nec iaculis libero porttitor. Fusce ac ultricies justo.</p>
 
         <div class="btn-group">
@@ -73,6 +74,127 @@
 </div>
 <!-- End jumbotron -->
 
+
+<!-- gallery section -->
+<div class="container">
+    <section>
+        <div class="page-header" id="gallery">
+            <h2>Gallery.<small>Check out our gallery!</small></h2>
+        </div>
+
+        <div class="carousel slide" id="screenshot-carousel" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#screenshot-carousel" data-slide-to="0" class="active"></li>
+                <li data-target="#screenshot-carousel" data-slide-to="1"></li>
+                <li data-target="#screenshot-carousel" data-slide-to="2"></li>
+                <li data-target="#screenshot-carousel" data-slide-to="3"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="item ">
+                    <img src="images/office-cleaning.jpg" alt="office-leaning">
+                    <div class="carousel-caption">
+                        <h2>Office cleaninig</h2>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="images/london-apartment-cleaning-service.jpg" alt="apartment-cleaning">
+                    <div class="carousel-caption">
+                        <h2>Apartment cleaninig</h2>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="images/restaurant-cleaning.jpg" alt="restaurant-cleaning">
+                    <div class="carousel-caption">
+                        <h2>Restaurant cleaning</h2>
+                    </div>
+                </div>
+                <div class="item active">
+                    <img src="images/cleaning-services-london.jpg" alt="cleaning-services-london">
+                    <div class="carousel-caption">
+                        <h2>Cleaning services in London!</h2>
+                    </div>
+                </div>
+            </div> <!-- end of carousel inner -->
+            <a href="#screenshot-carousel" class="left carousel-control" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+                </a>
+            <a href="#screenshot-carousel" class="right carousel-control" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+            </a>
+        </div> <!-- end carousel -->
+    </section>
+</div>
+<!-- Our job section -->
+<div class="container">
+    <section class="page-header" id="job">
+        <h2>What are we doing.. amazing stuff only!</h2>
+
+        <div class="row">
+            <div class="col-lg-8">
+                <p>We are cleaning carpets. After cleaning they looks like new!</p>
+            </div>
+            <div class="col-lg-4">
+                <img src="images/vacuum-carpet.jpeg" clss="img-responsive" alt="vaccum-carpet">
+            </div>
+        </div> <!-- end row -->
+        <hr>
+        <div class="row">
+            <div class="col-lg-8">
+                <p>We are cleaning carpets. After cleaning they looks like new!</p>
+            </div>
+            <div class="col-lg-4">
+                <img src="images/vacuum-carpet.jpeg" clss="img-responsive" alt="vaccum-carpet">
+            </div>
+        </div> <!-- end row -->
+        <hr>
+        <div class="row">
+            <div class="col-lg-8">
+                <p>We are cleaning carpets. After cleaning they looks like new!</p>
+            </div>
+            <div class="col-lg-4">
+                <img src="images/vacuum-carpet.jpeg" clss="img-responsive" alt="vaccum-carpet">
+            </div>
+        </div> <!-- end row -->
+
+        <hr>
+
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="panel panel-default text-center">
+                    <div class="panel-body">
+                        <span class="glyphicon glyphicon-fire"></span>
+                        <h4>This is a topic</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dignissim varius consequat. Cras in sodales felis. Cras efficitur elit neque, interdum varius massa vulputate sed. In a tempor felis, sit amet laoreet ipsum. Vivamus et faucibus ante, eget convallis nulla. Maecenas metus nulla, rhoncus pharetra lectus nec, posuere lacinia magna.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4">
+                <div class="panel panel-default text-center">
+                    <div class="panel-body">
+                        <span class="glyphicon glyphicon-fire"></span>
+                        <h4>This is a topic</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dignissim varius consequat. Cras in sodales felis. Cras efficitur elit neque, interdum varius massa vulputate sed. In a tempor felis, sit amet laoreet ipsum. Vivamus et faucibus ante, eget convallis nulla. Maecenas metus nulla, rhoncus pharetra lectus nec, posuere lacinia magna.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4">
+                <div class="panel panel-default text-center">
+                    <div class="panel-body">
+                        <span class="glyphicon glyphicon-fire"></span>
+                        <h4>This is a topic</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dignissim varius consequat. Cras in sodales felis. Cras efficitur elit neque, interdum varius massa vulputate sed. In a tempor felis, sit amet laoreet ipsum. Vivamus et faucibus ante, eget convallis nulla. Maecenas metus nulla, rhoncus pharetra lectus nec, posuere lacinia magna.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+</div> <!-- end of container -->
+
+<!-- feedback section -->
 <div class="container">
     <section>
         <div class="page-header" id="feedback">
@@ -131,57 +253,6 @@
     </div> <!-- end well -->
 
 </section> <!-- call to action section end -->
-
-<!-- gallery section -->
-<div class="container">
-    <section>
-        <div class="page-header" id="gallery">
-            <h2>Gallery.<small>Check out our gallery!</small></h2>
-        </div>
-
-        <div class="carousel slide" id="screenshot-carousel" data-ride="carousel">
-            <ol class="carousel-indicators">
-                <li data-target="#screenshot-carousel" data-slide-to="0" class="active"></li>
-                <li data-target="#screenshot-carousel" data-slide-to="1"></li>
-                <li data-target="#screenshot-carousel" data-slide-to="2"></li>
-                <li data-target="#screenshot-carousel" data-slide-to="3"></li>
-            </ol>
-            <div class="carousel-inner">
-                <div class="item ">
-                    <img src="images/office-cleaning.jpg" alt="office-leaning">
-                    <div class="carousel-caption">
-                        <h2>Office cleaninig</h2>
-                    </div>
-                </div>
-                <div class="item">
-                    <img src="images/london-apartment-cleaning-service.jpg" alt="apartment-cleaning">
-                    <div class="carousel-caption">
-                        <h2>Apartment cleaninig</h2>
-                    </div>
-                </div>
-                <div class="item">
-                    <img src="images/restaurant-cleaning.jpg" alt="restaurant-cleaning">
-                    <div class="carousel-caption">
-                        <h2>Restaurant cleaning</h2>
-                    </div>
-                </div>
-                <div class="item active">
-                    <img src="images/cleaning-services-london.jpg" alt="cleaning-services-london">
-                    <div class="carousel-caption">
-                        <h2>Cleaning services in London!</h2>
-                    </div>
-                </div>
-            </div> <!-- end of carousel inner -->
-            <a href="#screenshot-carousel" class="left carousel-control" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left"></span>
-                </a>
-            <a href="#screenshot-carousel" class="right carousel-control" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right"></span>
-            </a>
-        </div> <!-- end carousel -->
-    </section>
-</div>
-
     <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
 
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
